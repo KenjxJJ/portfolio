436a001ea3616319835c9d8b0b6096020cf7f93f
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-10-15 23:54:29 +0300

    Complete Home Page#1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-// import './App.css';   import css file later
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/app.css';  
+import JoelImg from './assets/img/jj.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header className="masthead mb-auto">
+      <div className="inner">
+        <h3 className="masthead-brand">Joel Kiggwe</h3>
+        <nav className="nav nav-masthead justify-content-center">
+          <a className="nav-link active" href="#">Home</a>
+          <a className="nav-link" href="#">Projects</a>
+          <a className="nav-link" href="#">Contact Me</a>
+        </nav>
+      </div>
+    </header>
+  
+    <main role="main" className="inner cover">
+       <img src={JoelImg} alt="Joel Kiggwe,a Front End Developer"/>
+      <h1 className="cover-heading">Front End Developer</h1>
+      <p>Are you looking for an expert in the mobile web design, or an awesome web app with performance in mind. 
+        You've met the right person. Try me and click the next button. </p>
+       <p className="lead">
+        <a href="#" className="btn btn-lg btn-secondary">Hire Me Now!</a>
+      </p>
+    </main>
+  
+    <footer className="mastfoot mt-auto">
+      <div className="inner">
+        <p>Developed by <span>Joel</span>. Check the <a href="https://github.com/KenjxJJ/"> GitHub Repo</a></p>
+      </div>
+    </footer>
     </div>
   );
 }
