ce889bb13c20322293e3ecdd52cb7ef8485ced0d
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-10-17 02:53:05 +0300

    Edit the Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,27 @@ import {Link}  from 'react-router-dom';
 
 const Home = () => {
   return (
-    <main role="main" className="inner cover">
+    <div className="homeBackground">
+    <main role="main" className="content inner cover mt-4 p-4 d-md-flex flex-md-row">
+        <div className="landing_image flex-md-grow-1 order-md-2 p-4">
       <img src={JoelImg} alt="Joel Kiggwe,a Front End Developer" />
-      <h1 className="cover-heading">Front End Developer</h1>
-      <p>
+      </div>
+      <div className="landing_text flex-md-grow-1 p-3 text-md-left">
+      <h1 className="cover-heading p-1">Front End Developer</h1>
+      
+      <p className="p-2">
         Are you looking for an expert in the mobile web design, or an awesome
         web app with performance in mind. You've met the right person. Try me
         and click the next button.{" "}
       </p>
-      <p className="lead">
-        <Link to="#" className="btn btn-lg btn-secondary">
+      <p className="lead pl-md-3">
+        <Link to="/contactme" className="btn btn-lg btn-secondary">
           Hire Me Now!
         </Link>
       </p>
+      </div>
     </main>
+    </div>
   );
 };
 
